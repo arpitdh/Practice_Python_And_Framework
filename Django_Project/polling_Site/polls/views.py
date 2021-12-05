c6263873771e05f98ddfb11a29a0516883beb513
Arpit Dhingra <arpitd22@gmail.com>
2021-12-05 09:39:14 +0530

    Implement URLs and Used Views

--- Django_Project/polling_Site/polls/views.py
+++ Django_Project/polling_Site/polls/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Question
 # Create your views here.
+
+def index(request):
+    latest_questions=Question.objects.order_by('-pub_date')[:5]
+    output="\n".join(q.question_text for q in latest_questions)
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse('This is a detail view of question %s'% question_id)
+
+def results (request,question_id):
+    return HttpResponse('These are the results %s' % question_id)
+
+def vote(request,question_id):
+    return HttpResponse('Votes on Question %s'% question_id)
