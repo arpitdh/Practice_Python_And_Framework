45cbb76605d5ba7433b6e284020afdf4e99b42fe
Arpit Dhingra <arpitd22@gmail.com>
2021-12-09 15:27:11 +0530

    Create FizzyBuzzy.py

---
+++ FizzyBuzzy.py
@@ -0,0 +1,5 @@
+"""Dodo's friend Dino challenged him to a new game he had invented - FizzyBuzzy. In this game, players are given a number N and they've to do the following:
+- print the numbers from 1 to N, each on a new line,
+- but for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+- For numbers which are multiples of both three and five print “FizzBuzz”. 
+Can you help Dodo win the match?"""
